ecba67536ff54c7bce47a46e2ef53e48d578528f
Ashok_Pal <ashok1717pal@gmail.com>
2023-11-25 13:50:37 +0000

    Gesture detector completed.

--- lib/main.dart
+++ lib/main.dart
@@ -23,25 +23,30 @@ class MyApp extends StatelessWidget {
               ),
               backgroundColor: Colors.grey,
             ),
-            body: Stack(
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  height: 300,
-                  width: 300,
-                  color: Colors.deepPurple,
-                ),
-                Container(
-                  height: 200,
-                  width: 200,
-                  color: Colors.deepPurple[400],
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.deepPurple[200],
-                ),
-              ],
+            body: Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => {print('yeet!')},
+                    child: Container(
+                      height: 300,
+                      width: 300,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.deepPurple[400],
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.deepPurple[200],
+                  ),
+                ],
+              ),
             )));
   }
 }
